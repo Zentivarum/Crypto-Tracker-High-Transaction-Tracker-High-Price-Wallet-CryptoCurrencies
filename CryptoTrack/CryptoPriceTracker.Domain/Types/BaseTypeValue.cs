470ab4afcc0a6b73218a3102198e5b28a8e8db22
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:26 +0300

    BaseTypeValue.cs

---
+++ CryptoTrack/CryptoPriceTracker.Domain/Types/BaseTypeValue.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoPriceTracker.Domain.Types;
+
+public abstract class BaseTypeValue
+{
+    protected BaseTypeValue(string value)
+    {
+        Value = value;
+    }
+    
+    public string Value { get; }
+}
