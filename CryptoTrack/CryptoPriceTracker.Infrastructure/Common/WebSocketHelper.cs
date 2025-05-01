8caf00e80ecf16715f4cbb95ce4e6c10d643acc3
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:33 +0300

    WebSocketHelper.cs

---
+++ CryptoTrack/CryptoPriceTracker.Infrastructure/Common/WebSocketHelper.cs
@@ -0,0 +1,15 @@
+﻿using CryptoExchange.Net.Objects.Sockets;
+
+namespace CryptoPriceTracker.Infrastructure.Common;
+
+public static class WebSocketHelper
+{
+    public static async Task Unsubscribe(string key, Dictionary<string, UpdateSubscription> socketList)
+    {
+        var isSuc = socketList.TryGetValue(key, out var websocket);
+        if(isSuc == false) return; 
+        
+        await websocket!.CloseAsync();
+        socketList.Remove(key);
+    }
+}
