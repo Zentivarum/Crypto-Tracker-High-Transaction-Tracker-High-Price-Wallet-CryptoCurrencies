74ebdcf1d774858948f015b142f18c6c59a7e963
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:30 +0300

    ProtocolTypes.cs

---
+++ CryptoTrack/CryptoPriceTracker.Domain/Types/ProtocolTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoPriceTracker.Domain.Types;
+
+public sealed class ProtocolTypes : BaseTypeValue
+{
+    private ProtocolTypes(string protocol) : base(protocol) { }
+
+    public static ProtocolTypes WebSocket = new ProtocolTypes("WebSocket");
+    public static ProtocolTypes Rest = new ProtocolTypes("Rest");
+}
